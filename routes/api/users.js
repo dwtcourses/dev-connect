1788c75f32427e35de4c06f631a25ff3636f0c29
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-09-30 12:43:56 -0400

    Updated User model.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
+const User = require("../../models/User");
 
 // @route      POST api/users
 // @desc       Register user
@@ -15,12 +16,28 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    // See if user exists
+    const { name, email, password } = req.body;
+
+    try {
+      let user = User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      // Get user gravatar
+      // Encrypt password
+      // Return jsonwebtoken
+      res.send("User route");
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Servor error");
+    }
   }
 );
 
