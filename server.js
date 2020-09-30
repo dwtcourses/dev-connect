c5aa756b00e170133f7854054fc7cfcac8fcdad6
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-09-30 11:56:25 -0400

    Added auth, post, profile, and users route. Hooked up mongoDB. Routes have been tested with Postman

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+// Connect DB.
+connectDB();
+
+// Init middleware.
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 
+// Define routes.
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
